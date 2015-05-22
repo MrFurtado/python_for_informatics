b6496964831f5ec719398d6f52a5895d3cbc2603
Jesse Neal <jesseneal@me.com>
2015-05-22 10:01:02 -0500

    Complete chap4 exercise6

---
+++ chap4/exercise6.py
@@ -0,0 +1,15 @@
+def computepay(hours, rate):
+  
+  if hours > 40:
+    pay = rate * 40 + (rate * 1.5 * (hours - 40))
+    print pay
+  else:
+    pay = rate * hours
+    print pay
+    
+
+prompt1 = raw_input('Enter hours: ')
+hours = float(prompt1)
+prompt2 = raw_input('Enter Rate: ')
+rate = float(prompt2)
+computepay(hours, rate)
