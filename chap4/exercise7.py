b4cb9495f0691d221f15c70cccfc9819b6bf39ed
Jesse Neal <jesseneal@me.com>
2015-05-22 10:10:01 -0500

    Complete chap4 exercise 7

---
+++ chap4/exercise7.py
@@ -0,0 +1,19 @@
+def computegrade(score):
+  try:
+      score = float(prompt1)
+      if score >= 0.9:
+          print 'A'
+      elif score >= 0.8:
+          print 'B'
+      elif score >= 0.7:
+          print 'C'
+      elif score >= 0.6:
+          print 'D'
+      else:
+          print 'F'
+  except:
+      print 'Bad score'
+
+prompt1 = raw_input("Enter score ")
+score = prompt1
+computegrade(score)
