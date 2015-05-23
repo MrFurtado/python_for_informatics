9f8f89157495dfe1d9cedd906516820d468f5747
Jesse Neal <jesseneal@me.com>
2015-05-22 20:39:47 -0500

    Complete Chap5 exercise 1

--- chap5/exercise1.py
+++ chap5/exercise1.py
@@ -3,13 +3,19 @@
 # numbers. If the user enters anything other than a number, detect their
 # mistake using try and except and print an error message and skop to the next
 # number.
+count = 0
 total = 0
 while True:
-	prompt1 = raw_input("Enter a number: ")
-	if prompt1 == 'done':
-		break
-	num = float(prompt1)
-	total = total + 1
-
-
-print total
+    prompt1 = raw_input("Enter a number: ")
+    if prompt1 == 'done':
+        break
+    try: 
+        num = float(prompt1)
+    except:
+        print "Invalid input"
+        continue
+    count = count + 1
+    total = total + num
+    print num, total, count
+    
+print 'Average: ', total / count
