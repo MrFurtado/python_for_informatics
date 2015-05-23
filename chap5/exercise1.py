ccff46baea36de99495a0b35710c2bf72114f46b
Jesse Neal <jesseneal@me.com>
2015-05-22 20:13:18 -0500

    Add comment to chap5 ex1

--- chap5/exercise1.py
+++ chap5/exercise1.py
@@ -1,3 +1,8 @@
+# Write a program which repeatedly reads number until the user enters "done".
+# Once "done" is entered, print out the total, count, and average of the
+# numbers. If the user enters anything other than a number, detect their
+# mistake using try and except and print an error message and skop to the next
+# number.
 total = 0
 while True:
 	prompt1 = raw_input("Enter a number: ")
