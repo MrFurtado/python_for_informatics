650a0a16fa7a9c31096d3caf36ec6c6eddf896bd
Jesse Neal <jneal@dfwnoc2.(none)>
2015-05-23 08:59:35 -0500

    complete chap5 exercise2

--- chap5/exercise2.py
+++ chap5/exercise2.py
@@ -1,1 +1,13 @@
-# Write another program that prompts for a list of numbers like chap 5 exercise 1 and at the end prints out both the maximum and minimum of the numbers instead of the average
+# Write another program that prompts for a list of numbers like chap 5
+# exercise 1 and at the end prints out both the maximum and minimum of the
+# numbers instead of the average
+num_array = []
+while True:
+	prompt1 = raw_input("Enter a number (tpye 'done' to stop): ")
+	if prompt1 == 'done':
+		break
+	num = int(prompt1)
+	num_array.append(num)
+
+print 'Minimum is: ', min(num_array)
+print 'Maximum is: ', max(num_array)
